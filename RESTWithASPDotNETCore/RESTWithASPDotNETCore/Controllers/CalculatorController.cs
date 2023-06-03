899f35d8db288a9855e06ac6a417b95026c1578d
davidsonromero <davikromero@hotmail.com>
2023-06-02 22:45:45 -0300

    Calculator

---
+++ RESTWithASPDotNETCore/RESTWithASPDotNETCore/Controllers/CalculatorController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTWithASPDotNETCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(Util.Util.IsNumeric(firstNumber) && Util.Util.IsNumeric(secondNumber))
+            {
+                decimal sum = Util.Util.ConvertToDecimal(firstNumber) + Util.Util.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input!");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (Util.Util.IsNumeric(firstNumber) && Util.Util.IsNumeric(secondNumber))
+            {
+                decimal sum = Util.Util.ConvertToDecimal(firstNumber) - Util.Util.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input!");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (Util.Util.IsNumeric(firstNumber) && Util.Util.IsNumeric(secondNumber))
+            {
+                decimal sum = Util.Util.ConvertToDecimal(firstNumber) * Util.Util.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input!");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (Util.Util.IsNumeric(firstNumber) && Util.Util.IsNumeric(secondNumber))
+            {
+                decimal sum = Util.Util.ConvertToDecimal(firstNumber) / Util.Util.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input!");
+        }
+
+        [HttpGet("sqrt/{num}")]
+        public IActionResult Sqrt(string num)
+        {
+            if (Util.Util.IsNumeric(num))
+            {
+                double sum = Math.Sqrt(Util.Util.ConvertToDouble(num));
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input!");
+        }
+    }
+}
---
+++ RESTWithASPDotNETCore/RESTWithASPDotNETCore/Util/Util.cs
@@ -0,0 +1,41 @@
+﻿namespace RESTWithASPDotNETCore.Util
+{
+    public sealed class Util
+    {
+        public static decimal ConvertToDecimal(string number)
+        {
+            if (decimal.TryParse(number,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimal result))
+            {
+                return result;
+            }
+            return result;
+        }
+
+        public static double ConvertToDouble(string number)
+        {
+            if (double.TryParse(number,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out double result))
+            {
+                return result;
+            }
+            return result;
+        }
+
+        public static bool IsNumeric(string number)
+        {
+            if (decimal.TryParse(number,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimal result))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
