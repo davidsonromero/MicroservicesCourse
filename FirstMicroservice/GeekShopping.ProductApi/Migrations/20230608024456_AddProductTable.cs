38c88205c8f7f19b7d5a94136a6cbcef01157464
davidsonromero <davikromero@hotmail.com>
2023-06-07 23:55:40 -0300

    Creation of First Microservice

---
+++ FirstMicroservice/GeekShopping.ProductApi/Migrations/20230608024456_AddProductTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PRODUCT",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NAME = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PRICE = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DESCRIPTION = table.Column<string>(type: "varchar(1000)", maxLength: 1000, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CATEGORY_NAME = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IMAGE_URL = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUCT", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PRODUCT");
+        }
+    }
+}
---
+++ FirstMicroservice/GeekShopping.ProductApi/Model/Product.cs
@@ -0,0 +1,35 @@
+﻿using GeekShopping.ProductApi.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductApi.Model
+{
+    [Table("PRODUCT")]
+    public class Product : BaseEntity
+    {
+        [Column("NAME")]
+        [Required]
+        [StringLength(150)]
+        public string? Name { get; set; }
+
+        [Column("PRICE")]
+        [Required]
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+
+        [Column("DESCRIPTION")]
+        [Required]
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Column("CATEGORY_NAME")]
+        [Required]
+        [StringLength(100)]
+        public string? CategoryName { get; set; }
+
+        [Column("IMAGE_URL")]
+        [Required]
+        [StringLength(300)]
+        public string? ImageUrl { get; set; }
+    }
+}
